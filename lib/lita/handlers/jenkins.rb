abe0d2dd7fc627e2520905024bf6572369fd4da2
Daniel <daniel.kiros@gmail.com>
2014-03-10 09:52:09 +0800

    get job list from configured url

--- lib/lita/handlers/jenkins.rb
+++ lib/lita/handlers/jenkins.rb
@@ -1,12 +1,51 @@
+require 'json'
+
 module Lita
   module Handlers
     class Jenkins < Handler
+      def self.default_config(config)
+        config.url = nil
+      end
+
       route /j(?:enkins)? list( (.+))?/i, :jenkins_list, command: true, help: {
         'jekins list <filter>' => 'lists Jenkins jobs'
       }
 
       def jenkins_list(response)
-        filter = response.matches.first.first.downcase
+        url             = Lita.config.handlers.jenkins.url
+        path            = "#{url}/api/json"
+        filter          = response.matches.first.last
+
+        api_response    = http.get(path)
+        parsed_response = JSON.parse(api_response.body)
+        reply = ''
+
+        parsed_response['jobs'].each_with_index do |job, i|
+          job_name      = job['name']
+          state         = color_to_state(job['color'])
+          text_to_check = state + job_name
+
+          reply << "[#{i+1}] #{state} #{job_name}\n" if filter_match(filter, text_to_check)
+        end
+
+        response.reply reply
+      end
+
+      private
+
+      def color_to_state(text)
+        case text
+        when /disabled/
+          'DISA'
+        when /red/
+          'FAIL'
+        else
+          'SUCC'
+        end
+      end
+
+      def filter_match(filter, text)
+        text.match(/#{filter}/i)
       end
     end
 
--- spec/lita/handlers/jenkins_spec.rb
+++ spec/lita/handlers/jenkins_spec.rb
@@ -3,4 +3,42 @@ require "spec_helper"
 describe Lita::Handlers::Jenkins, lita_handler: true do
   it { routes_command('jenkins list').to(:jenkins_list) }
   it { routes_command('jenkins list filter').to(:jenkins_list) }
+
+  describe '#jenkins list' do
+    let(:response) { double("Faraday::Response") }
+    let(:api_response) { %{
+      {"assignedLabels":[{}],"mode":"NORMAL","nodeDescription":"the master Jenkins node",
+      "nodeName":"","numExecutors":4,"description":null,"jobs":[
+      {"name":"chef_converge", "url":"http://test.com/job/chef_converge/","color":"disabled"},
+      {"name":"deploy", "url":"http://test.com/job/deploy/","color":"red"},
+      {"name":"build-all", "url":"http://test.com/job/build-all/","color":"blue"},
+      {"name":"website", "url":"http://test.com/job/website/","color":"red"}],
+      "overallLoad":{},
+      "primaryView":{"name":"All","url":"http://test.com/"},"quietingDown":false,"slaveAgentPort":8090,"unlabeledLoad":{},
+      "useCrumbs":false,"useSecurity":true,"views":[
+      {"name":"All","url":"http://test.com/"},
+      {"name":"Chef","url":"http://test.com/view/Chef/"},
+      {"name":"Deploy","url":"http://test.com/view/Deploy/"},
+      {"name":"Status","url":"http://test.com/view/Status/"},
+      {"name":"deploy-pipeline","url":"http://test.com/view/deploy-pipeline/"}]}
+    } }
+
+    before do
+      allow_any_instance_of(Faraday::Connection).to receive(:get).and_return(response)
+    end
+
+    it 'lists all jenkins jobs' do
+      allow(response).to receive(:status).and_return(200)
+      allow(response).to receive(:body).and_return(api_response)
+      send_command('jenkins list')
+      expect(replies.last).to eq("[1] DISA chef_converge\n[2] FAIL deploy\n[3] SUCC build-all\n[4] FAIL website\n")
+    end
+
+    it 'filters jobs' do
+      allow(response).to receive(:status).and_return(200)
+      allow(response).to receive(:body).and_return(api_response)
+      send_command('jenkins list fail')
+      expect(replies.last).to eq("[2] FAIL deploy\n[4] FAIL website\n")
+    end
+  end
 end
