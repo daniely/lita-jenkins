6a34032ba19076d6fa807ff6e1de28ebc9f759c0
Daniel <daniel.kiros@gmail.com>
2014-03-10 22:10:18 +0800

    basic implementation of building jenkins job by ID

--- lib/lita/handlers/jenkins.rb
+++ lib/lita/handlers/jenkins.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'base64'
 
 module Lita
   module Handlers
@@ -15,6 +16,36 @@ module Lita
       route /j(?:enkins)? list( (.+))?/i, :jenkins_list, command: true, help: {
         'jekins list <filter>' => 'lists Jenkins jobs'
       }
+
+      route /j(?:enkins)? b (\d+)/i, :jenkins_build_by_id, command: true, help: {
+        'jekins b <job_id>' => 'builds the job specified by job_id. List jobs to get ID.'
+      }
+
+      def jenkins_build_by_id(response)
+        job = jobs[response.matches.last.last.to_i - 1]
+
+        if job
+          jenkins_build(response, job['name'])
+        else
+          response.reply "I couldn't find that job. Try `jenkins list` to get a list."
+        end
+      end
+
+      def jenkins_build(response, job_name=nil)
+        url    = Lita.config.handlers.jenkins.url
+        auth64 = Base64.encode64(Lita.config.handlers.jenkins.auth)
+        path   = "#{url}/job/#{job_name}/build"
+
+        http_resp = http.post(path) do |req|
+          req.headers['Authorization'] = "Basic #{auth64}"
+        end
+
+        if http_resp.status == 201
+          response.reply "(#{http_resp.status}) Build started for #{job_name} #{url}/job/#{job_name}"
+        else
+          response.reply http_resp.body
+        end
+      end
 
       def jenkins_list(response)
         url             = Lita.config.handlers.jenkins.url
